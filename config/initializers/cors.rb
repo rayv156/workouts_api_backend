139528d1b9708821000812485c613d3e8e0d5c3c
Ray Velasquez <rayvelasquez@Rays-MacBook-Pro.local>
2020-12-10 22:11:24 -0600

    added specific url to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://naughty-yalow-c5e710.netlify.app/'
 
     resource '*',
       headers: :any,
